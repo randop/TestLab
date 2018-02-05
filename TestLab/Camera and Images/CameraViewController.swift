ef844fc5385434fad5b41ef066cd06f4e28a5795
Randolph <randop@me.com>
2018-02-05 22:48:59 +0800

    Capture a picture from Camera or Photo Library

---
+++ TestLab/Camera and Images/CameraViewController.swift
@@ -0,0 +1,75 @@
+//
+//  CameraViewController.swift
+//  TestLab
+//
+//  Created by Randolph on 2/5/18.
+//  Copyright © 2018 Randolph. All rights reserved.
+//
+
+import UIKit
+
+class CameraViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    @IBOutlet weak var picture: UIImageView!
+    
+    @IBAction func cameraTap(_ sender: Any) {
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            let imagePicker = UIImagePickerController()
+            imagePicker.delegate = self
+            imagePicker.sourceType = .camera;
+            imagePicker.allowsEditing = false
+            self.present(imagePicker, animated: true, completion: nil)
+        }
+    }
+
+    @IBAction func photoTap(_ sender: Any) {
+        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
+            let imagePicker = UIImagePickerController()
+            imagePicker.delegate = self
+            imagePicker.sourceType = .photoLibrary;
+            imagePicker.allowsEditing = true
+            self.present(imagePicker, animated: true, completion: nil)
+        }
+    }
+    
+    @IBAction func uploadTap(_ sender: Any) {
+        if let image = picture.image {
+            let imageData = UIImageJPEGRepresentation(image, 0.7)
+            let compressedJPGImage = UIImage(data: imageData!)
+            //TODO
+        } else {
+            let alert = UIAlertController(title: nil, message: "Please select a picture from Camera or Photo Library", preferredStyle: .alert)
+            let ok = UIAlertAction(title: "OK", style: .default, handler: nil)
+            alert.addAction(ok)
+            self.present(alert, animated: true, completion: nil)
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        let image = info[UIImagePickerControllerOriginalImage] as! UIImage
+        picture.image = image
+        dismiss(animated:true, completion: nil)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
