daec64013733bdec871856383192a110cf11933e
Randolph <randop@me.com>
2018-07-24 17:01:55 +0800

    small tweaks

--- TestLab/Controllers/List and Tables/PictureDetailViewController.swift
+++ TestLab/Controllers/List and Tables/PictureDetailViewController.swift
@@ -8,8 +8,9 @@
 
 import UIKit
 
-class PictureDetailViewController: UIViewController {
+class PictureDetailViewController: UIViewController, UIScrollViewDelegate {
 
+    @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var imageView: UIImageView!
     
     var selectedImage: String?
@@ -17,11 +18,25 @@ class PictureDetailViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        title = selectedImage
+        
         if let imageToLoad = selectedImage {
             imageView.image  = UIImage(named: imageToLoad)
         }
 
-        // Do any additional setup after loading the view.
+        scrollView.delegate = self
+        scrollView.minimumZoomScale = 1.0
+        scrollView.maximumZoomScale = 10.0
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.hidesBarsOnTap = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.hidesBarsOnTap = false
     }
 
     override func didReceiveMemoryWarning() {
@@ -29,15 +44,8 @@ class PictureDetailViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return imageView
     }
-    */
 
 }
